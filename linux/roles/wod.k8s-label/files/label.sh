7af965030a75facb3fef57f59024ce7b85f6df4f
mengkzhaoyun <mengkzhaoyun@gmail.com>
2018-09-18 16:18:50 +0800

    update

--- linux/roles/wod.k8s-label/files/label.sh
+++ linux/roles/wod.k8s-label/files/label.sh
@@ -8,7 +8,7 @@ K8S_ROLE="${K8S_ROLE:-worker}"
 mkdir -p /opt/cni/bin
 
 if [ $K8S_ROLE == "master" ] ; then
-  kubectl label node $K8S_HOST node-role.kubernetes.io/master="true" --overwrite
+  /opt/bin/kubectl label node $K8S_HOST node-role.kubernetes.io/master="true" --overwrite
 else
-  kubectl label node $K8S_HOST node-role.kubernetes.io/node="true" --overwrite
+  /opt/bin/kubectl label node $K8S_HOST node-role.kubernetes.io/node="true" --overwrite
 fi
