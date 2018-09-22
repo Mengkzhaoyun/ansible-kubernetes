44d25b66809493d4597e2ed4627af8479423e5f5
shiyao <shiyao@spacesystech.com>
2018-09-22 15:53:48 +0800

    update

--- linux/roles/wod.k8s-traefik/templates/treafik.sh
+++ linux/roles/wod.k8s-traefik/templates/treafik.sh
@@ -12,6 +12,6 @@ if ! [[ -e /etc/kubernetes/helm/traefik/Chart.yaml ]]; then
   docker pull $REGISTRY_LOCAL$REGISTRY_TRAEFIK_CHART_REPO:$REGISTRY_TRAEFIK_CHART_VERSION
   docker run -v /etc/kubernetes/helm/traefik:/data/output --rm $REGISTRY_LOCAL$REGISTRY_TRAEFIK_CHART_REPO:$REGISTRY_TRAEFIK_CHART_VERSION
   helm install /etc/kubernetes/helm/traefik --name traefik --namespace kube-system \
-  --set image.repository={{ REGISTRY_LOCAL }}{{ CLOUD_IMAGES['TRAEFIK-NODE']['NAME'] }} \
-  --set image.tag={{ CLOUD_IMAGES['TRAEFIK-NODE']['VERSION'] }} \
+  --set image.repository={{ REGISTRY_LOCAL }}{{ CLOUD_IMAGES['TRAEFIK']['NAME'] }} \
+  --set image.tag={{ CLOUD_IMAGES['TRAEFIK']['VERSION'] }} \
 fi
