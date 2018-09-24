f41cc9ccdcdad39282d63a2850aeaac87153459c
shiyao <shiyao@spacesystech.com>
2018-09-24 21:02:19 +0800

    update

---
+++ linux/roles/wod.k8s-cloud/templates/cloud.sh
@@ -0,0 +1,52 @@
+#!/bin/bash 
+
+set -e  
+
+REGISTRY_LOCAL="{{ REGISTRY_LOCAL }}"
+REGISTRY_CLOUD_CHART_REPO="{{ CLOUD_IMAGES['CLOUD-CHART']['NAME'] }}"
+REGISTRY_CLOUD_CHART_VERSION="{{ CLOUD_IMAGES['CLOUD-CHART']['VERSION'] }}"
+
+mkdir -p /etc/kubernetes/helm
+
+if ! [[ -e /etc/kubernetes/helm/cloud/Chart.yaml ]]; then
+  docker pull $REGISTRY_LOCAL$REGISTRY_CLOUD_CHART_REPO:$REGISTRY_CLOUD_CHART_VERSION
+  docker run -v /etc/kubernetes/helm/cloud:/data/output --rm $REGISTRY_LOCAL$REGISTRY_CLOUD_CHART_REPO:$REGISTRY_CLOUD_CHART_VERSION
+  helm install /etc/kubernetes/helm/cloud --name cloud --namespace cloud \
+  --set host="{{ CLOUD['HOST'] }}" \
+  --set gitlab.host="https://{{ GITLAB['HOST'] }}" \
+  --set redis.repository={{ REGISTRY_LOCAL }}{{ CLOUD_IMAGES['CLOUD-REDIS']['NAME'] }} \
+  --set redis.tag={{ CLOUD_IMAGES['CLOUD-REDIS']['VERSION'] }} \
+  --set mysql.password="{{ CLOUD['MYSQL']['PASSWORD'] }}" \
+  --set mysql.port={{ CLOUD['MYSQL']['PORT'] }} \
+  --set mysql.image.repository={{ CLOUD_IMAGES['CLOUD-MYSQL']['NAME'] }} \
+  --set mysql.image.tag={{ CLOUD_IMAGES['CLOUD-MYSQL']['VERSION'] }} \
+  --set mysql.initImage.repository={{ CLOUD_IMAGES['CLOUD-INITMYSQL']['NAME'] }} \
+  --set mysql.initImage.tag={{ CLOUD_IMAGES['CLOUD-INITMYSQL']['VERSION'] }} \
+  --set dex.clientID="{{ CLOUD['DEX']['CLIENTID'] }}" \
+  --set dex.clientSecret="{{ CLOUD['DEX']['CLIENTSECRET'] }}" \
+  --set dex.k8sSecret="{{ CLOUD['DEX']['K8SSECRET'] }}" \
+  --set harbor.password="{{ HARBOR['PASSWORD'] }}" \
+  --set harbor.registory="{{ HARBOR['HOST'] }}" \
+  --set k8s.password="{{ K8S_ADMIN_PWD }}" \
+  --set drone.server="{{ DRONE['HOST'] }}" \
+  --set drone.token="{{ CLOUD['DRONE_TOKEN'] }}" \
+  --set awecloud.dex.image.repository={{ CLOUD_IMAGES['CLOUD-AWECLOUD-DEX']['NAME'] }} \
+  --set awecloud.dex.image.tag={{ CLOUD_IMAGES['CLOUD-AWECLOUD-DEX']['VERSION'] }} \
+  --set awecloud.api.image.repository={{ CLOUD_IMAGES['CLOUD-AWECLOUD-API']['NAME'] }} \
+  --set awecloud.api.image.tag={{ CLOUD_IMAGES['CLOUD-AWECLOUD-API']['VERSION'] }} \
+  --set awecloud.deploy.image.repository={{ CLOUD_IMAGES['CLOUD-AWECLOUD-DEPLOY']['NAME'] }} \
+  --set awecloud.deploy.image.tag={{ CLOUD_IMAGES['CLOUD-AWECLOUD-DEPLOY']['VERSION'] }} \
+  --set awecloud.harbor.image.repository={{ CLOUD_IMAGES['CLOUD-AWECLOUD-HARBOR']['NAME'] }} \
+  --set awecloud.harbor.image.tag={{ CLOUD_IMAGES['CLOUD-AWECLOUD-HARBOR']['VERSION'] }} \
+  --set awecloud.kubernetes.image.repository={{ CLOUD_IMAGES['CLOUD-AWECLOUD-KUBERNETES']['NAME'] }} \
+  --set awecloud.kubernetes.image.tag={{ CLOUD_IMAGES['CLOUD-AWECLOUD-KUBERNETES']['VERSION'] }} \
+  --set awecloud.rest.image.repository={{ CLOUD_IMAGES['CLOUD-AWECLOUD-REST']['NAME'] }} \
+  --set awecloud.rest.image.tag={{ CLOUD_IMAGES['CLOUD-AWECLOUD-REST']['VERSION'] }} \
+  --set awecloud.login.image.repository={{ CLOUD_IMAGES['CLOUD-AWECLOUD-LOGIN']['NAME'] }} \
+  --set awecloud.login.image.tag={{ CLOUD_IMAGES['CLOUD-AWECLOUD-LOGIN']['VERSION'] }} \
+  --set awecloud.ui.image.repository={{ CLOUD_IMAGES['CLOUD-AWECLOUD-UI']['NAME'] }} \
+  --set awecloud.ui.image.tag={{ CLOUD_IMAGES['CLOUD-AWECLOUD-UI']['VERSION'] }} \
+  --set awecloud.users.image.repository={{ CLOUD_IMAGES['CLOUD-AWECLOUD-USERS']['NAME'] }} \
+  --set awecloud.users.image.tag={{ CLOUD_IMAGES['CLOUD-AWECLOUD-USERS']['VERSION'] }}
+fi
+
