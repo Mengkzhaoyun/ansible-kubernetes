adc7526c4f33547608ad19d3dfa0684a81da127e
shiyao <shiyao@spacesystech.com>
2018-10-08 09:45:41 +0800

    update

--- linux/roles/wod.k8s-cni/files/cni.sh
+++ linux/roles/wod.k8s-cni/files/cni.sh
@@ -11,7 +11,7 @@ if [[ -e /opt/cni/bin/host-local ]]; then
   echo 'cni bin is already exist!'
 else
   curl $HTTP_SERVER/$K8S_CNI_BIN.tgz > /opt/cni/bin/$K8S_CNI_BIN.tgz
-  cd /opt/cni/bin && tar -xvf /opt/cni/bin/$K8S_CNI_BIN.tgz
+  cd /opt/cni/bin && tar -xzf /opt/cni/bin/$K8S_CNI_BIN.tgz
   rm -rf /opt/cni/bin/$K8S_CNI_BIN.tgz
   echo 'cni bin download completed!'
 fi
