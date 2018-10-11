60403f0ffdc9b9fd0c2583bc6de08d54569e9d0a
gaoshiyao <gaoshiyao@wodcloud.com>
2018-10-11 11:44:37 +0800

    update

--- linux/roles/wod.k8s-drone/templates/drone.sh
+++ linux/roles/wod.k8s-drone/templates/drone.sh
@@ -12,8 +12,8 @@ if ! [[ -e /etc/kubernetes/helm/drone/Chart.yaml ]]; then
   docker pull $REGISTRY_LOCAL$REGISTRY_DRONE_CHART_REPO:$REGISTRY_DRONE_CHART_VERSION
   docker run -v /etc/kubernetes/helm/drone:/data/output --rm $REGISTRY_LOCAL$REGISTRY_DRONE_CHART_REPO:$REGISTRY_DRONE_CHART_VERSION
   helm install /etc/kubernetes/helm/drone --name drone --namespace devops \
-  --set gitlab.host="https://{{ GITLAB['HOST'] }}" \
-  --set drone.host="https://{{ DRONE['HOST'] }}" \
+  --set gitlab.host="{{ GITLAB['HOST'] }}" \
+  --set drone.host="{{ DRONE['HOST'] }}" \
   --set drone.server.image.repository={{ REGISTRY_LOCAL }}{{ CLOUD_IMAGES['DRONE-SERVER']['NAME'] }} \
   --set drone.server.image.tag={{ CLOUD_IMAGES['DRONE-SERVER']['VERSION'] }} \
   --set drone.agent.image.repository={{ REGISTRY_LOCAL }}{{ CLOUD_IMAGES['DRONE-AGENT']['NAME'] }} \
