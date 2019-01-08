8d2d11e72a980b62a08db9d0f707f004006f5291
shucheng <shucheng@spacesystech.com>
2019-01-08 22:02:42 +0800

    update

--- linux/roles/wod.registry/files/prepare.sh
+++ linux/roles/wod.registry/files/prepare.sh
@@ -3,7 +3,7 @@
 set -e
 
 HTTP_SERVER="${HTTP_SERVER:-https://dl.wodcloud.com/k8s}"
-RKT_ACI_REGISTRY="${RKT_ACI_REGISTRY:-registry-2.6.2}"
+RKT_ACI_REGISTRY="${RKT_ACI_REGISTRY:-registry-2.6.2.aci}"
 RKT_ACI_REGISTRY_DATA="${RKT_ACI_REGISTRY_DATA:-registry-data-v1.9.0}"
 REGISTRY_REMOTE="${REGISTRY_REMOTE:-}"
 REGISTRY_REMOTE_SPLIT="${REGISTRY_REMOTE_SPLIT:-}"
@@ -13,12 +13,12 @@ REGISTRY_VERSION_REGISTRY="${REGISTRY_VERSION_REGISTRY:-}"
 mkdir -p /etc/kubernetes/downloads
 mkdir -p /etc/kubernetes/data
 
-if [[ -e /etc/kubernetes/downloads/$RKT_ACI_REGISTRY.aci ]]; then
+if [[ -e /etc/kubernetes/downloads/$RKT_ACI_REGISTRY ]]; then
 	echo 'registry aci is already exist!'
 else
   if [[ -n "${REGISTRY_REMOTE:-}" ]]; then
     rkt --insecure-options=image fetch docker://$REGISTRY_REMOTE$REGISTRY_REPO_REGISTRY$REGISTRY_REMOTE_SPLIT$REGISTRY_VERSION_REGISTRY
-    rkt image export $REGISTRY_REMOTE$REGISTRY_REPO_REGISTRY$REGISTRY_REMOTE_SPLIT$REGISTRY_VERSION_REGISTRY /etc/kubernetes/downloads/$REGISTRY_REPO_REGISTRY-$REGISTRY_VERSION_REGISTRY.aci
+    rkt image export $REGISTRY_REMOTE$REGISTRY_REPO_REGISTRY$REGISTRY_REMOTE_SPLIT$REGISTRY_VERSION_REGISTRY /etc/kubernetes/downloads/$REGISTRY_REPO_REGISTRY-$REGISTRY_VERSION_REGISTRY
   else
     curl $HTTP_SERVER/$RKT_ACI_REGISTRY.tgz > /etc/kubernetes/downloads/$RKT_ACI_REGISTRY.tgz
     cd /etc/kubernetes/downloads && tar -xzf /etc/kubernetes/downloads/$RKT_ACI_REGISTRY.tgz
