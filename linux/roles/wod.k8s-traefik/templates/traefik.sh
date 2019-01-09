9eb44acd6ea085f5773e6e30cfbd47c6ae5a9fb6
mengkzhaoyun <mengkzhaoyun@gmail.com>
2019-01-09 16:47:23 +0800

    update

--- linux/roles/wod.k8s-traefik/templates/traefik.sh
+++ linux/roles/wod.k8s-traefik/templates/traefik.sh
@@ -3,8 +3,8 @@
 set -e  
 
 REGISTRY_LOCAL="{{ REGISTRY_LOCAL }}"
-REGISTRY_TRAEFIK_CHART_REPO="{{ CLOUD_IMAGES['TRAEFIK-CHART']['NAME'] }}"
-REGISTRY_TRAEFIK_CHART_VERSION="{{ CLOUD_IMAGES['TRAEFIK-CHART']['VERSION'] }}"
+REGISTRY_TRAEFIK_CHART_REPO="{{ K8S_IMAGES['TRAEFIK-CHART']['NAME'] }}"
+REGISTRY_TRAEFIK_CHART_VERSION="{{ K8S_IMAGES['TRAEFIK-CHART']['VERSION'] }}"
 
 mkdir -p /etc/kubernetes/helm
 
@@ -12,6 +12,6 @@ if ! [[ -e /etc/kubernetes/helm/traefik/Chart.yaml ]]; then
   docker pull $REGISTRY_LOCAL$REGISTRY_TRAEFIK_CHART_REPO:$REGISTRY_TRAEFIK_CHART_VERSION
   docker run -v /etc/kubernetes/helm/traefik:/data/output --rm $REGISTRY_LOCAL$REGISTRY_TRAEFIK_CHART_REPO:$REGISTRY_TRAEFIK_CHART_VERSION
   helm install /etc/kubernetes/helm/traefik --name traefik --namespace kube-system \
-  --set image.repository={{ REGISTRY_LOCAL }}{{ CLOUD_IMAGES['TRAEFIK']['NAME'] }} \
-  --set image.tag={{ CLOUD_IMAGES['TRAEFIK']['VERSION'] }} 
+  --set image.repository={{ REGISTRY_LOCAL }}{{ K8S_IMAGES['TRAEFIK']['NAME'] }} \
+  --set image.tag={{ K8S_IMAGES['TRAEFIK']['VERSION'] }} 
 fi
