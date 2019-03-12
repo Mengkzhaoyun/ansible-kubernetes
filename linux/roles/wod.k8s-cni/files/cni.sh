c2d23a7361fa6887bf95b9af540b49db90134d1e
mengkzhaoyun <mengkzhaoyun@gmail.com>
2019-03-12 16:02:45 +0800

    update

--- linux/roles/wod.k8s-cni/files/cni.sh
+++ linux/roles/wod.k8s-cni/files/cni.sh
@@ -5,7 +5,10 @@ set -e
 HTTP_SERVER="${HTTP_SERVER:-https://dl.wodcloud.com/k8s}"
 K8S_CNI_BIN="${K8S_CNI_BIN:-cni-plugins-amd64-v0.7.0}"
 
-cat > /etc/sysconfig/modules/ipvs.modules <<EOF
+if [[ -e /etc/sysconfig/modules/ipvs.modules ]]; then
+  echo 'ipvs.modules is already exist!'
+else
+  cat > /etc/sysconfig/modules/ipvs.modules <<EOF
 #!/bin/bash
 ipvs_modules="ip_vs ip_vs_lc ip_vs_wlc ip_vs_rr ip_vs_wrr ip_vs_lblc ip_vs_lblcr ip_vs_dh ip_vs_sh ip_vs_fo ip_vs_nq ip_vs_sed ip_vs_ftp nf_conntrack_ipv4"
 for kernel_module in \${ipvs_modules}; do
@@ -15,8 +18,9 @@ for kernel_module in \${ipvs_modules}; do
     fi
 done
 EOF
-
-chmod 755 /etc/sysconfig/modules/ipvs.modules && bash /etc/sysconfig/modules/ipvs.modules && lsmod | grep ip_vs
+  chmod 755 /etc/sysconfig/modules/ipvs.modules && bash /etc/sysconfig/modules/ipvs.modules && lsmod | grep ip_vs
+  echo 'ipvs.modules config completed!'
+fi
 
 mkdir -p /opt/cni/bin
 
