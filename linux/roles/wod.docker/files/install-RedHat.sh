40be370194cf70cf481f35596cceaf9d04e5ee38
mengkzhaoyun <mengkzhaoyun@gmail.com>
2019-04-24 11:34:41 +0800

    update

--- linux/roles/wod.docker/files/install-RedHat.sh
+++ linux/roles/wod.docker/files/install-RedHat.sh
@@ -3,5 +3,9 @@
 set -e  
 
 if ! [ -x "$(command -v docker)" ]; then
-  curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
+  yum install -y yum-utils device-mapper-persistent-data lvm2
+  yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+  yum makecache fast
+  yum -y install docker-ce
+  service docker start
 fi
