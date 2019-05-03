075b2f01d357d39152a7cd8f529b5bd413a3cf25
mengkzhaoyun <mengkzhaoyun@gmail.com>
2019-05-03 10:33:48 +0800

    update

--- linux/roles/wod.k8s-master/files/scripts/kubelet.sh
+++ linux/roles/wod.k8s-master/files/scripts/kubelet.sh
@@ -70,6 +70,10 @@ run ${RKT_RUN_ARGS} \
 --volume coreos-var-log,kind=host,source=/var/log,readOnly=false \
 --volume coreos-os-release,kind=host,source=/etc/os-release,readOnly=true \
 --volume coreos-run,kind=host,source=/run,readOnly=false \
+--volume coreos-bin,kind=host,source=/bin,readOnly=true \
+--volume coreos-sbin,kind=host,source=/sbin,readOnly=true \
+--volume coreos-usr-bin,kind=host,source=/usr/bin,readOnly=true \
+--volume coreos-usr-sbin,kind=host,source=/usr/sbin,readOnly=true \
 --volume coreos-lib-modules,kind=host,source=/lib/modules,readOnly=true \
 --volume coreos-etc-cni-net,kind=host,source=/etc/cni/net.d,readOnly=true \
 --volume coreos-var-lib-cni,kind=host,source=/var/lib/cni,readOnly=false \
@@ -83,6 +87,10 @@ run ${RKT_RUN_ARGS} \
 --mount volume=coreos-var-log,target=/var/log \
 --mount volume=coreos-os-release,target=/etc/os-release \
 --mount volume=coreos-run,target=/run \
+--mount volume=coreos-bin,target=/bin \
+--mount volume=coreos-sbin,target=/sbin \
+--mount volume=coreos-usr-bin,target=/usr/bin \
+--mount volume=coreos-usr-sbin,target=/usr/sbin \
 --mount volume=coreos-lib-modules,target=/lib/modules \
 --mount volume=coreos-etc-cni-net,target=/etc/cni/net.d \
 --mount volume=coreos-var-lib-cni,target=/var/lib/cni \
