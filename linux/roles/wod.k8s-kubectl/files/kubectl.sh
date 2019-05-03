7f7c8364e8ec58863622f6ca45fa9d2fd74e9371
shucheng <shucheng@spacesystech.com>
2019-05-03 22:37:34 +0800

    update

--- linux/roles/wod.k8s-kubectl/files/kubectl.sh
+++ linux/roles/wod.k8s-kubectl/files/kubectl.sh
@@ -12,22 +12,21 @@ TOOLS_KUBECTL="${TOOLS_KUBECTL:-${REGISTRY_KUBECTL_REPO}-${REGISTRY_KUBECTL_VERS
 mkdir -p /etc/kubernetes/downloads
 mkdir -p /opt/bin
 
-if ! [ -x "$(command -v kubectl)" ]; then
-  if ! [[ -e /etc/kubernetes/downloads/$TOOLS_KUBECTL ]]; then
-    if [[ -n "${REGISTRY_REMOTE:-}" ]]; then
-      docker run -v /etc/kubernetes/downloads:/data/output --rm $REGISTRY_REMOTE$REGISTRY_KUBECTL_REPO$REGISTRY_REMOTE_SPLIT$REGISTRY_KUBECTL_VERSION
-    else
-      curl $HTTP_SERVER/$TOOLS_KUBECTL.tgz > /etc/kubernetes/downloads/$TOOLS_KUBECTL.tgz
-    fi 
-    cd /etc/kubernetes/downloads && tar -xzf /etc/kubernetes/downloads/$TOOLS_KUBECTL.tgz
-    rm -rf /etc/kubernetes/downloads/$TOOLS_KUBECTL.tgz 
-  fi
+if ! [[ -e /etc/kubernetes/downloads/$TOOLS_KUBECTL ]]; then
+
+  docker run -v /etc/kubernetes/downloads:/data/output --rm $REGISTRY_REMOTE$REGISTRY_KUBECTL_REPO$REGISTRY_REMOTE_SPLIT$REGISTRY_KUBECTL_VERSION
+
+  cd /etc/kubernetes/downloads && tar -xzf /etc/kubernetes/downloads/$TOOLS_KUBECTL.tgz
+
+  rm -rf /etc/kubernetes/downloads/$TOOLS_KUBECTL.tgz 
+
   chmod 0744 /etc/kubernetes/downloads/$TOOLS_KUBECTL
   rm -rf /opt/bin/kubectl
-  ln -s /etc/kubernetes/downloads/$TOOLS_KUBECTL /opt/bin/kubectl
+  ln -s /etc/kubernetes/downloads/$TOOLS_KUBECTL /opt/bin/kubectl  
+
   /opt/bin/kubectl config set-cluster kubernetes --server=http://127.0.0.1:8080
   /opt/bin/kubectl config set-context kubernetes --cluster=kubernetes
-  /opt/bin/kubectl config use-context kubernetes
+  /opt/bin/kubectl config use-context kubernetes      
 fi
 
 if ! [ -x "$(command -v kubectl)" ]; then
