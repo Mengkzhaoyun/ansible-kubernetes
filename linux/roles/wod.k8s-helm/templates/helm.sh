ab6d860e0678494ada6ae0ad4ce9e9ce75447288
shucheng <shucheng@spacesystech.com>
2019-05-03 22:35:27 +0800

    update

--- linux/roles/wod.k8s-helm/templates/helm.sh
+++ linux/roles/wod.k8s-helm/templates/helm.sh
@@ -13,20 +13,23 @@ REGISTRY_TILLER_VERSION="{{ K8S_IMAGES['TILLER']['VERSION'] }}"
 mkdir -p /etc/kubernetes/downloads
 mkdir -p /opt/bin
 
-if ! [ -x "$(command -v helm)" ]; then
-  if ! [[ -e /etc/kubernetes/downloads/$TOOLS_HELM ]]; then
-    docker run -v /etc/kubernetes/downloads:/data/output --rm $REGISTRY_LOCAL$REGISTRY_HELM_REPO:$REGISTRY_HELM_VERSION
-    cd /etc/kubernetes/downloads && tar -xzf /etc/kubernetes/downloads/$TOOLS_HELM.tgz
-    rm -rf /etc/kubernetes/downloads/$TOOLS_HELM.tgz
-  fi
+if ! [[ -e /etc/kubernetes/downloads/$TOOLS_HELM ]]; then
+
+  docker run -v /etc/kubernetes/downloads:/data/output --rm $REGISTRY_LOCAL$REGISTRY_HELM_REPO:$REGISTRY_HELM_VERSION
+  cd /etc/kubernetes/downloads && tar -xzf /etc/kubernetes/downloads/$TOOLS_HELM.tgz
+  rm -rf /etc/kubernetes/downloads/$TOOLS_HELM.tgz
+
   chmod 0744 /etc/kubernetes/downloads/$TOOLS_HELM
   rm -rf /opt/bin/helm
   ln -s /etc/kubernetes/downloads/$TOOLS_HELM /opt/bin/helm
+
   if ! [[ -e /etc/kubernetes/addons/kube-system/rbac-tiller.yml ]]; then
     /opt/bin/kubectl create serviceaccount --namespace kube-system tiller
     /opt/bin/kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-  fi
-  /opt/bin/helm init --upgrade -i $REGISTRY_LOCAL$REGISTRY_TILLER_REPO:$REGISTRY_TILLER_VERSION --service-account tiller --stable-repo-url $HTTP_SERVER/charts
+  fi    
+
+  /opt/bin/helm init --upgrade -i $REGISTRY_LOCAL$REGISTRY_TILLER_REPO:$REGISTRY_TILLER_VERSION --service-account tiller --stable-repo-url $HTTP_SERVER/charts    
+
 fi
 
 if ! [ -x "$(command -v helm)" ]; then
