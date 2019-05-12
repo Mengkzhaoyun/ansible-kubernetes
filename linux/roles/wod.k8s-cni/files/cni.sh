2c5537c6f4c90ab4556e7c21d6091057ae1d8013
shucheng <shucheng@spacesystech.com>
2019-05-12 11:06:42 +0800

    update

--- linux/roles/wod.k8s-cni/files/cni.sh
+++ linux/roles/wod.k8s-cni/files/cni.sh
@@ -10,15 +10,13 @@ if [[ -e /etc/sysconfig/modules/ipvs.modules ]]; then
 else
   cat > /etc/sysconfig/modules/ipvs.modules <<EOF
 #!/bin/bash
-ipvs_modules="ip_vs ip_vs_lc ip_vs_wlc ip_vs_rr ip_vs_wrr ip_vs_lblc ip_vs_lblcr ip_vs_dh ip_vs_sh ip_vs_fo ip_vs_nq ip_vs_sed ip_vs_ftp nf_conntrack_ipv4"
-for kernel_module in \${ipvs_modules}; do
-    /sbin/modinfo -F filename \${kernel_module} > /dev/null 2>&1
-    if [ $? -eq 0 ]; then
-        /sbin/modprobe \${kernel_module}
-    fi
-done
+modprobe -- ip_vs
+modprobe -- ip_vs_rr
+modprobe -- ip_vs_wrr
+modprobe -- ip_vs_sh
+modprobe -- nf_conntrack_ipv4
 EOF
-  chmod 755 /etc/sysconfig/modules/ipvs.modules && bash /etc/sysconfig/modules/ipvs.modules && lsmod | grep ip_vs
+  chmod 755 /etc/sysconfig/modules/ipvs.modules && bash /etc/sysconfig/modules/ipvs.modules && lsmod | grep -e ip_vs -e nf_conntrack_ipv4
   echo 'ipvs.modules config completed!'
 fi
 
