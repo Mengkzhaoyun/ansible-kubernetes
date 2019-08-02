2cb780aaff03b1acc97f885510ea3e10bc62ca61
wanyunjia <wanyunjia@bd-apaas.com>
2019-08-02 18:28:06 +0800

    auto certificate

--- linux/roles/wod.k8s-ssl/templates/gencert.sh
+++ linux/roles/wod.k8s-ssl/templates/gencert.sh
@@ -1,17 +1,17 @@
 #!/bin/bash  
 set -e 
  
-if [[ -e /etc/kubernetes/ssl/{{ K8S_DOMAIN }}.key ]]; then
+if [[ -e /etc/kubernetes/ssl/{{ K8S_DOMAIN }}/{{ K8S_DOMAIN }}.key ]]; then
   echo "{{ K8S_DOMAIN }}.key is ready!"
 else
   /usr/bin/openssl genrsa -out {{ K8S_DOMAIN }}.key 2048
   echo "{{ K8S_DOMAIN }}.key is ready!"
 fi
 
-if [[ -e /etc/kubernetes/ssl/{{ K8S_DOMAIN }}.crt ]]; then
+if [[ -e /etc/kubernetes/ssl/{{ K8S_DOMAIN }}/{{ K8S_DOMAIN }}.crt ]]; then
   echo "{{ K8S_DOMAIN }}.crt is ready!"
 else
   /usr/bin/openssl req -new -key {{ K8S_DOMAIN }}.key -out {{ K8S_DOMAIN }}.csr -subj "/CN=admin/C=CN/ST=BeiJing/L=Beijing/O=system:masters/OU=System" -config {{ K8S_DOMAIN }}.cnf
-  /usr/bin/openssl x509 -req -in {{ K8S_DOMAIN }}.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out {{ K8S_DOMAIN }}.crt -days 3650 -extensions v3_req -extfile {{ K8S_DOMAIN }}.cnf
+  /usr/bin/openssl x509 -req -in {{ K8S_DOMAIN }}.csr -CA ../ca.crt -CAkey ../ca.key -CAcreateserial -out {{ K8S_DOMAIN }}.crt -days 3650 -extensions v3_req -extfile {{ K8S_DOMAIN }}.cnf
   echo "{{ K8S_DOMAIN }}.crt is ready!"
 fi
